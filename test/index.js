5f621b8f13094a77d54fcf82c999f12b3d278e6f
lucasurzedo <lucasurzedo1@gmail.com>
2020-11-13 08:38:43 -0300

    feat: adiciona threads para realizar tasks

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const anyjs = require('../src/index.js');
-const path = require('path');
+const instantiate = require('./instantiate.js');
+const execute = require('./execute.js');
 
 const { Worker, isMainThread, parentPort, workerData} = require('worker_threads');
 
@@ -20,79 +20,9 @@ const object = {
     "year": 2009
 }
 
-function tasksThreaded(){
-
-    let i = 0;
-    let workerPath;
-    let json;
-
-    if(isMainThread){
-        for (i; i < 2; i++) {
-            if (i % 2 == 0) {
-                workerPath = path.resolve('test/instantiate.js');
-                json = object;
-            }
-            else {
-                workerPath = path.resolve('test/execute.js');
-                json = jsonFunc
-            }
-            const worker = new Worker(workerPath, { workerData : json})
-
-            worker.on('message', (message) =>{
-                console.log(message);
-            });
-
-            worker.on('error', console.error);
-
-            worker.on('exit', code => {
-                if (code !== 0) console.log(new Error(`Worker stopped with error code ${code}`));
-            })
-        }
-    }
-};
+async function tasksThreaded(){
+    execute(jsonFunc).then(result => console.log(result));
+    instantiate(object).then(result => console.log(result));
+}
 
 tasksThreaded();
-
-// async function testeBubbleSort(input) {
-//     let machines = await anyjs.startAnyJS();
-
-//     let tickets = [];
-//     for (let index = 0; index < 100; index++) {
-//         tickets[index] = await anyjs.executeAccess(input, machines);
-//         console.log(tickets[index]);
-//     }
-    
-//     for (let index = 99; index >= 0; index--) {
-//         console.log(await anyjs.getResult(tickets[index]));
-//     }
-
-//     anyjs.endServer();
-// }
-
-// async function instantiateVar(object) {
-//     let machines = await anyjs.startAnyJS();
-
-//     let result;
-//     let cont = 0;
-//     while(cont < 10){
-//         let nickname = "example" + cont;
-//         object.nickname = nickname;
-//         result = await anyjs.instantiateObject(object, machines);
-//         console.log(result);
-//         cont++;
-//     }
-
-//     cont = 0;
-//     while(cont < 10){
-//         const nickname = {
-//             "nickname" : "example" + cont
-//         }
-//         result = await anyjs.getObject(nickname, machines);
-//         console.log(result);
-//         cont++;
-//     }
-
-//     anyjs.endServer();
-// }
-
-
